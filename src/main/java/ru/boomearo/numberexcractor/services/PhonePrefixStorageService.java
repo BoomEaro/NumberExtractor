7f6f181ab686b1541e5bda68202482cbbfc312cb
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-06-14 16:25:10 +0300

    Implement phone prefixes parser

--- src/main/java/ru/boomearo/numberexcractor/services/PhonePrefixStorageService.java
+++ src/main/java/ru/boomearo/numberexcractor/services/PhonePrefixStorageService.java
@@ -2,15 +2,30 @@ package ru.boomearo.numberexcractor.services;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+import ru.boomearo.numberexcractor.utils.StringParserUtils;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+@Slf4j
 @Service
 public class PhonePrefixStorageService {
 
-    private ConcurrentMap<Integer, Country> countriesByPrefix = new ConcurrentHashMap<>();
+    private final ConcurrentMap<Integer, Country> countriesByPrefix;
+
+    private static final String PARSE_URL = "https://en.wikipedia.org/wiki/List_of_country_calling_codes";
+
+    public PhonePrefixStorageService() {
+        this.countriesByPrefix = parsePrefixes();
+
+        log.info("Найдено " + this.countriesByPrefix.size() + " международных кодов.");
+    }
 
     public String getCountryNameByPrefix(int prefix) {
         Country country = this.countriesByPrefix.get(prefix);
@@ -18,6 +33,52 @@ public class PhonePrefixStorageService {
             return "Unknown";
         }
         return country.getName();
+    }
+
+    private static ConcurrentMap<Integer, Country> parsePrefixes() {
+        ConcurrentMap<Integer, Country> countriesTmp = new ConcurrentHashMap<>();
+        try {
+            Document doc = Jsoup.connect(PARSE_URL).get();
+            Elements tables = doc.select("table.wikitable");
+            Element rows = tables.get(1);
+
+            Elements data = rows.select("tr");
+            for (Element r : data) {
+                Elements e = r.select("td");
+                if (e.size() < 2) {
+                    continue;
+                }
+                Element elementPrefix = e.get(1);
+
+                //Убираем лишние символы при попытке извлечь код страны
+                String prefixString = elementPrefix.text()
+                        .replace("+", "")
+                        .replace("[notes 1]", "")
+                        .replace("was ", "")
+                        .replace("assigned ", "")
+                        .replace(" ", "");
+
+                //На википедии в таблице под одной страной может быть намного больше кодов.
+                String[] splitPrefixes = prefixString.split(",");
+                for (String prefix : splitPrefixes) {
+                    Integer prefixNum = StringParserUtils.parseIntegerSafe(prefix);
+                    if (prefixNum == null) {
+                        continue;
+                    }
+
+                    Element elementCountry = e.get(0);
+
+                    String country = elementCountry.text();
+
+                    countriesTmp.put(prefixNum, new Country(prefixNum, country));
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return countriesTmp;
     }
 
     @Data
