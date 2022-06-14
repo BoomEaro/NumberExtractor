632ce287d9b4be47e3e8cb0afcf34ef475f9dc82
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-06-14 13:11:18 +0300

    Initial commit

---
+++ src/main/java/ru/boomearo/numberexcractor/NumberExtractorApplication.java
@@ -0,0 +1,13 @@
+package ru.boomearo.numberexcractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberExtractorApplication.class, args);
+	}
+
+}
