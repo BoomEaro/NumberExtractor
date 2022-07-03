62a46f61367c5308259b341d205fdc23341d8735
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-07-03 19:34:40 +0300

    Fix test, remove unused test

--- src/test/java/ru/boomearo/numberextractor/services/ExtractorCountryServiceTest.java
+++ src/test/java/ru/boomearo/numberextractor/services/ExtractorCountryServiceTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.boomearo.numberextractor.dto.ErrorData;
 import ru.boomearo.numberextractor.dto.ExtractorCountryRequest;
 import ru.boomearo.numberextractor.dto.ExtractorCountryResponse;
+import ru.boomearo.numberextractor.parser.phone.PhonePrefixParser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
@@ -19,28 +19,24 @@ public class ExtractorCountryServiceTest {
     @Mock
     private CountryCodeStorageService countryCodeStorageService;
 
+    @Mock
+    private PhonePrefixParser phonePrefixParser;
+
     @InjectMocks
     private ExtractorCountryService countryService;
 
     @Test
-    public void shouldReturnCorrectCountryName() {
+    public void shouldReturnCorrectCountryName() throws Exception {
         when(countryCodeStorageService.getCountryNameByCode(371)).thenReturn("Latvia");
+        when(phonePrefixParser.parsePhonePrefix("37112345678")).thenReturn(371);
+
         ExtractorCountryResponse response =
                 this.countryService.extractCountry(new ExtractorCountryRequest("37112345678"));
 
         verify(countryCodeStorageService).getCountryNameByCode(371);
+        verify(phonePrefixParser).parsePhonePrefix("37112345678");
 
-        ExtractorCountryResponse expected = new ExtractorCountryResponse(new ExtractorCountryResponse.CountryData("Latvia", "+37112345678", 371));
-
-        assertEquals(expected, response);
-    }
-
-    @Test
-    public void shouldReturnError() {
-        ExtractorCountryResponse response =
-                this.countryService.extractCountry(new ExtractorCountryRequest("WrongName"));
-
-        ExtractorCountryResponse expected = new ExtractorCountryResponse(new ErrorData("The string supplied did not seem to be a phone number."));
+        ExtractorCountryResponse expected = new ExtractorCountryResponse(new ExtractorCountryResponse.CountryData("Latvia", "37112345678", 371));
 
         assertEquals(expected, response);
     }
